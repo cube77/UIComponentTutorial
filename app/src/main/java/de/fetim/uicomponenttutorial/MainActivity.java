1f2f3fc855ade786808a9d2826c469ce94929da4
Fetim <fazlija@fetim.de>
2017-01-02 23:36:49 +0100

    added functionality from chapter 3.2 , Die UI-Komponenten von Material Design Six items arranging via xml file

--- app/src/main/java/de/fetim/uicomponenttutorial/MainActivity.java
+++ app/src/main/java/de/fetim/uicomponenttutorial/MainActivity.java
@@ -2,6 +2,10 @@ package de.fetim.uicomponenttutorial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((Button)findViewById(R.id.button)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println(((Switch)findViewById(R.id.switch1)).isChecked() ? "An" : "Aus");
+                System.out.println(((EditText)findViewById(R.id.editText4)).getText());
+            }
+        });
     }
 }
