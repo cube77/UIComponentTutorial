7473a0d8e87ae93d889f235f735de2ac12355229
Fetim Fazlija <fazlija@fetim.de>
2017-01-02 19:31:00 +0100

    Initial commit

---
+++ app/src/main/java/de/fetim/uicomponenttutorial/MainActivity.java
@@ -0,0 +1,13 @@
+package de.fetim.uicomponenttutorial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
